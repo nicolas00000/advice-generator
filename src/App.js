b8c37d25f6197391cc50e2640a2f3d8f5d3c526b
nicolas00000 <nicmariano10@gmail.com>
2023-06-18 13:17:00 -0300

    add functions and render card

--- src/App.js
+++ src/App.js
@@ -5,24 +5,79 @@ import './styles.css';
 
 function App() {
   const [Number, setNumber] = useState(0)
-  const [Conselho, setConselho] = useState("")
+  const [Conselho, setConselho] = useState("Clique aqui para ver o seu conselho")
+  const url = `https://api.adviceslip.com/advice`
+
+  function gerarConselho() {
+      fetch(url)
+          .then(response => {
+              return response.json()
+          })
+          .then(adviceData => {
+              const Adviceobj = adviceData.slip
+              setNumber(Adviceobj.id)
+              traduzir(Adviceobj.advice)
+          })
+          .catch(error => {
+              console.log(error)
+          })
+
+      function traduzir(texto){
+        fetch(
+          `https://api.mymemory.translated.net/get?q=${texto}!&langpair=en|pt`
+        ).then((resp) => resp.json()).then((data) => {
+          if(data.responseStatus === 429){
+            setConselho(texto)
+            console.log("não foi possivel traduzir devido ao limite de requisições diario")
+          }else{
+            setConselho( data.responseData.translatedText )
+          }
+        }).catch(
+          setConselho(texto)
+        )
+      }
+  }
+
+  function copyTextToClipboard() {
+    navigator.clipboard.writeText(Conselho)
+      .then(() => {
+        console.log('Texto copiado: ' + Conselho);
+      })
+      .catch((error) => {
+        console.error('Erro ao copiar o texto:', error);
+      });
+  }
+  
+  function falar(){
+    let utterance = new
+    SpeechSynthesisUtterance(Conselho)
+    speechSynthesis.speak(utterance)
+  }
+
   return (
     <div class="card">
       <div class="card-body">
       <h5 class="card-title">Conselho - {Number}</h5>
       <p class="card-text">
-        loremdojsaodjsaojdojsoajdojsaodjosjodsdasdsadsad jadosjojsaodjo jasojdo ajodj oajs0odj 0ojaj
-        With supporting text below as  a natural lead-in to additional content.</p>
+        {Conselho}
+      </p>
       <div class="button-position">
-          <button id="sort">Button</button>
+          <button id="sort" onClick={() => gerarConselho()}>
+            <i class="fa-solid fa-dice"></i>
+          </button>
       </div>
       <div className='botoes'>
-        <div class="microfone">mic</div>
-        <div class="copy">cop</div>
+        <div class="microfone" onClick={()=> falar()}>
+          <i class="fa-solid fa-microphone"></i>
+        </div>
+        <div class="copy">
+        <i class="fa-solid fa-copy" onClick={() => copyTextToClipboard()}></i>
+        </div>
       </div>
       </div>
     </div>
   );
+  
 }
 
 export default App;
