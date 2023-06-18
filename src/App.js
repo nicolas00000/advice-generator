780c443eb612a6350c4bdde396b0c6f0c7b9ccf0
nicolas00000 <nicmariano10@gmail.com>
2023-06-18 12:08:43 -0300

    :pencil: render card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
+import './styles.css';
+
 
 function App() {
+  const [Number, setNumber] = useState(0)
+  const [Conselho, setConselho] = useState("")
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div class="card">
+      <div class="card-body">
+      <h5 class="card-title">Conselho - {Number}</h5>
+      <p class="card-text">
+        loremdojsaodjsaojdojsoajdojsaodjosjodsdasdsadsad jadosjojsaodjo jasojdo ajodj oajs0odj 0ojaj
+        With supporting text below as  a natural lead-in to additional content.</p>
+      <div class="button-position">
+          <button id="sort">Button</button>
+      </div>
+      <div className='botoes'>
+        <div class="microfone">mic</div>
+        <div class="copy">cop</div>
+      </div>
+      </div>
     </div>
   );
 }
